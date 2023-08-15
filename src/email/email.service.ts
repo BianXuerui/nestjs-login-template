4ced3d9a25548e5a8192552ac1540e57d7cfefc3
BianXuerui <407955981@qq.com>
2023-08-15 16:57:34 +0800

    feat: 注册帐号通过邮箱验证码

---
+++ src/email/email.service.ts
@@ -0,0 +1,32 @@
+import { Injectable } from '@nestjs/common';
+import { createTransport, Transport } from 'nodemailer';
+import { from } from 'rxjs';
+
+@Injectable()
+export class EmailService {
+  transporter: Transport;
+
+  constructor() {
+    this.transporter = createTransport({
+      host: 'smtp.qq.com',
+      port: 587,
+      secure: false,
+      auth: {
+        user: '407955981@qq.com',
+        pass: 'dcnlutzbtlxebjdd',
+      },
+    });
+  }
+
+  async sendMail({ to, subject, html }) {
+    await this.transporter.sendMail({
+      from: {
+        name: '单县湖西棋院课时系统',
+        address: '407955981@qq.com',
+      },
+      to,
+      subject,
+      html,
+    });
+  }
+}
